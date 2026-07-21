a5ac357ccfcb1fd8b454cf9e8663f2bdecaf0d9d
Yashaswini H S <yashuhsyashu12@gmail.com>
2026-07-21 20:48:39 +0530

    Time: 235 ms (94.75%) | Memory: 95.2 MB (56.49%) - LeetSync

---
+++ 2731-memoize/memoize.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const cache = new Map();
+
+    return function (...args) {
+        const key = JSON.stringify(args);
+
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+
+        const result = fn(...args);
+        cache.set(key, result);
+        return result;
+    };
+}
