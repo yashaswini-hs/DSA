ccef341b2232fa6e13b5318179fd8c1a0f39e467
Yashaswini H S <yashuhsyashu12@gmail.com>
2026-07-22 20:04:40 +0530

    Time: 819 ms (7.68%), Space: 0B (100%) - LeetHub

---
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -0,0 +1,9 @@
+SELECT
+    s1.score,
+    (
+        SELECT COUNT(DISTINCT s2.score)
+        FROM Scores s2
+        WHERE s2.score >= s1.score
+    ) AS `rank`
+FROM Scores s1
+ORDER BY s1.score DESC;
